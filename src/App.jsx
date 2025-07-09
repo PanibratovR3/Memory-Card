e6f13bdd7ca1f7c6939ea5f2e23ce013c4748486
PanibratovR3 <roman.panibratov@gmail.com>
2025-07-09 13:24:18 +0300

    Implemented functionality to upload Pokemon image from API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const results = useData("http://pokeapi.co/api/v2/pokemon/bulbasaur");
+  function useData(url) {
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      let ignore = false;
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          if (!ignore) {
+            setData(json.sprites.other["official-artwork"].front_default);
+          }
+        });
+      return () => {
+        ignore = true;
+      };
+    }, [url]);
+    return data;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <img src={results} />
+    </div>
+  );
 }
 
-export default App
+export default App;
